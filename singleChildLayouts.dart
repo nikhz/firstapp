98f00809e77e918b99a2cc932a53f488a5a4a1f6
nikhz <73939944+nikhz@users.noreply.github.com>
2021-03-09 11:04:47 +0530

    Add files via upload
    firs app-study purpose

---
+++ singleChildLayouts.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SingleChildLayouts extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      child: Image.asset("assets/car2.jpg"), //Text("Mallu Developer"),
+      alignment: Alignment.bottomCenter,
+    );
+  }
+
+}
